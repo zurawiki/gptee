8ae99f94e578b7f47e6bb6e1c5ce151f09084674
Roger Zurawicki <zurawiki@users.noreply.github.com>
2023-01-31 09:15:43 -0800

    Improve GPT2 tokenizer initialization
    - Refactor the GPT2 tokenizer initialization

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn model_name_to_context_size(model_name: &str) -> u16 {
     }
 }
 
-fn count_tokens(prompt: &str) -> anyhow::Result<u16> {
+fn get_tokenizer() -> anyhow::Result<Gpt2Tokenizer> {
     let mut vocab_file = tempfile::NamedTempFile::new()?;
     vocab_file.write_all(include_bytes!("../resources/encoder.json"))?;
     let vocab = Gpt2Vocab::from_file(vocab_file.into_temp_path()).unwrap();
@@ -30,20 +30,25 @@ fn count_tokens(prompt: &str) -> anyhow::Result<u16> {
     let merges = BpePairVocab::from_file(merges_file.into_temp_path()).unwrap();
 
     let lower_case = false;
-    let tokenizer = Gpt2Tokenizer::from_existing_vocab_and_merges(vocab, merges, lower_case);
+    Ok(Gpt2Tokenizer::from_existing_vocab_and_merges(
+        vocab, merges, lower_case,
+    ))
+}
+fn count_tokens(prompt: &str) -> anyhow::Result<u16> {
+    // TODO this function should not return result
+    let tokenizer = get_tokenizer()?;
     Ok(tokenizer.tokenize(prompt).len() as u16)
 }
 
 async fn prompt(client: &Client, prompt: &str) -> Result<String, Box<dyn Error>> {
     let model = std::env::var("OPENAI_MODEL").unwrap_or_else(|_| "text-ada-001".to_string());
 
-    dbg!(&model);
     let request = CreateCompletionRequestArgs::default()
         .model(&model)
         .prompt(prompt)
-        .max_tokens(dbg!(
+        .max_tokens(
             model_name_to_context_size(&model) - count_tokens(prompt)?
-        ))
+        )
         .build()?;
 
     let response = client.completions().create(request).await?;
